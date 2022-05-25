ab4d7fa0fde29a26d54efcbaeb7710a090226eb7
Ubaid-ch <80938435+Ubaid-ch@users.noreply.github.com>
2022-05-25 19:43:31 +0500

    Add files via upload

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Header(){
+    return(
+    <header className="header">
+        <h1 className="header-title">Sigma Memer</h1>
+        <h4 className="header-about">Meme-generator</h4>
+    </header>)
+}
